6c60ecd1240763873f9bd325b550eed37917d11f
ktsullivan0523 <ktsullivan@noctrl.edu>
2021-03-23 20:21:58 -0500

    figuring things out

--- Apps/Screens/SignInScreen.js
+++ Apps/Screens/SignInScreen.js
@@ -1,63 +1,30 @@
 import React from 'react';
-import {ImageBackground, StyleSheet, Text, View, TextInput, Keyboard, Button} from "react-native";
-import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
+import { StyleSheet, Text, View } from 'react-native';
 
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-function SignInScreen(props) {
-    const orientation = useDeviceOrientation();
+import Amplify from '@aws-amplify/core'
+import config from './aws-exports'
+Amplify.configure(config)
 
+class App extends React.Component {
+  render() {
     return (
-        <ImageBackground
-         style={styles.background} 
-         source={require('../assets/pawprints.jpg')}>
-            <View>
-                <Text style={[styles.heading, {marginTop: orientation.landscape ? 10 : "50%" }]}>Pet Patrol</Text>
-                
-                <TextInput style={styles.username} placeholder='username' onSubmitEditing={Keyboard.dismiss}/>
-                <TextInput style={styles.password} placeholder='password' onSubmitEditing={Keyboard.dismiss}/>
-                <Button title="Submit" />
-                <Button title="Sign-up" />
-            </View>
-            
-        </ImageBackground>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Yo!</Text>
+      </View>
     );
+  }
 }
 
+export default withAuthenticator(App, { includeGreetings: true })
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        resizeMode: 'contain'
-        
-    },
-    heading: {
-        fontSize: 75,
-        paddingVertical: 15,
-        textAlign: "center",
-        fontWeight: "bold"
-    },
-    password:{
-        marginTop: 55,
-        padding: 10,
-        borderWidth: 0.5,
-        borderRadius: 2,
-        backgroundColor: "#fff",
-        width: "65%",
-        //display: "flex",
-        marginRight: "auto",
-        marginLeft: "auto"
-    },
-    
-    username: {
-        marginTop: 50,
-        padding: 10,
-        borderWidth: 0.5,
-        borderRadius: 2,
-        backgroundColor: "#fff",
-        width: "65%",
-       // display: "block",
-        marginRight: "auto",
-        marginLeft: "auto"
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-export default SignInScreen;
